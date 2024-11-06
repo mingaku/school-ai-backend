fc742c52de477ac277ea534bed1471d0c85d69a1
Taro-oka <okamoto-1994@outlook.jp>
2024-11-06 09:08:50 +0900

    1000000に変更

--- controllers/azureAssistantApiController.js
+++ controllers/azureAssistantApiController.js
@@ -236,8 +236,8 @@ exports.createThreadAndRun = async (req, res) => {
         messages,
       },
       stream: true,
-      max_prompt_tokens: 50000,
-      max_completion_tokens: 50000,
+      max_prompt_tokens: 1000000,
+      max_completion_tokens: 1000000,
     });
 
     res.setHeader("Content-Type", "text/event-stream");
